8f6a3a0b471d04a0b248901d8de8abf64f4e2ec8
Priest98 <priest98@example.com>
2026-07-11 15:24:15 +0100

    Fix HealthModule missing dependencies

--- backend/src/health/health.module.ts
+++ backend/src/health/health.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TerminusModule } from '@nestjs/terminus';
 import { HealthController } from './health.controller';
 import { ProvidersModule } from '../providers/providers.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  imports: [ProvidersModule],
+  imports: [TerminusModule, ProvidersModule, PrismaModule],
   controllers: [HealthController],
 })
 export class HealthModule {}
